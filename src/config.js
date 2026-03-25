b0d37900a42437ba54ae0b795e12000fa10aa493
kaboremikael31-cpu <kaboremikael31@gmail.com>
2026-03-25 14:16:06 +0000

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  BotName: "Vendroz-MD", 
+  nameCreator: "Vendroz", 
+  Newsletter: "120363423530040334@newsletter",  // ← ICI : le JID de ta chaîne (format xxx@newsletter)
+  Newsletter2: "Vendroz-Tech",                  // ← Nom affiché de la chaîne (texte libre)
+  Newsletter3: "Rejoignez Vendroz-Tech pour les news exclusives !", // Message description
+  Newsletter4: "Updates bots, stickers, IA et fun 🔥",
+  Newsletter5: "Le channel officiel du bot Vendroz-MD",
+  Newsletter6: "https://whatsapp.com/channel/0029Vb75KOEKrWQt4TzvrR1k"  // ← ICI : le LIEN DIRECT de ta chaîne (pour les boutons/menu)
+};
